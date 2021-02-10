ecba4c82e1de46af05b93d628ef37b20a62d7adc
java-ninano <kgstnwls@naver.com>
2021-02-10 16:45:32 +0900

    202010210

--- src/main/java/org/zerock/mapper/MountainMapper.java
+++ src/main/java/org/zerock/mapper/MountainMapper.java
@@ -1,10 +1,22 @@
 package org.zerock.mapper;
 
+import java.util.List;
+
 import org.zerock.domain.MountainVO;
 
 public interface MountainMapper {
 
+	public List<MountainVO> getList();
+	
+	public void insert(MountainVO mountain);
+	
 	public void insertSelectKey(MountainVO mountain);
+	
+	public MountainVO read(Long no);
+	
+	public int delete(Long no);
+	
+	public int update(MountainVO mountain);
 
 	
 	
