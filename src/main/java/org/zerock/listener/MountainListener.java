fea9795c3faafbd771a1e8f510d4dc6e8bedc453
dmswldi <eeeunzz20@gmail.com>
2021-02-17 01:14:49 +0900

    0216

--- src/main/java/org/zerock/listener/MountainListener.java
+++ src/main/java/org/zerock/listener/MountainListener.java
@@ -32,6 +32,8 @@ public class MountainListener implements ServletContextListener {
     	// Context Root -> Application 영역에 추가
     	ServletContext application = sce.getServletContext();
     	application.setAttribute("root", application.getContextPath());
+    	
+    	application.setAttribute("staticPath", "https://objectstorage.ap-seoul-1.oraclecloud.com/n/cnkghg76rfem/b/bucket-20210216-1420/o");
     }
 	
 }
--- src/main/webapp/resources/js/notice/get.js
+++ src/main/webapp/resources/js/notice/get.js
@@ -93,7 +93,7 @@ $(function(){
 			$('#replyTable').empty();// 하위 요소 제거
 			
 			var now = new Date();
-			$.each(data, function(i, item){
+			$.each(data, function(i, item){// (index, element)
 				var diff = (now.getTime() - item.updatedate)/(1000*60*60);// 시간 단위
 				
 				if(diff < 1) {// 1시간 미만
@@ -101,25 +101,46 @@ $(function(){
 				} else if(diff < 24) {// 1시간 ~ 24시간
 					diff = Math.floor(diff) + '시간 전';
 				} else {// 24시간 이상
-					diff = Math.floor(diff*60) + '일 전';
+					diff = Math.floor(diff/24) + '일 전';
 				}
 			
 				$('#replyTable').append(
 					 '<tr>'
 					+	'<td>'
+					+	  '<div><input type="text" name="reply" value="' + item.reply + '" class="form-control replies" readonly>'
+					+	  '<input type="hidden" name="no" value="' + item.no + '" class="reply_no"></div>'
 					+	  '<div><strong>' + item.replyer + '</strong><small class="float-right">' + diff + '</small></div>'
-					+	  '<div>' + item.reply + '</div>'
+					+	  '<div>'
+					+		'<i class="fas fa-ellipsis-h"></i>'
+					+		'<a class="modReplyBtn mx-2"><small>수정</small></a>'
+					+		'<a class="delReplyBtn"><small>삭제</small></a>'
+					+		'<a class="cancelReplyBtn ml-2"><small>취소</small></a>'
+					+		'<a class="submitReplyBtn ml-2"><small>등록</small></a>'
+					+	  '</div>'
 					+	'</td>'
 					+ '</tr>'
 				
 				);
+				
+				//$('.modReplyBtn').hide();
+				//$('.delReplyBtn').hide();
+	
 			});
 			
 		});
 	};
 	
+	
 	getReplyList();
 	
+	
+	
+	$('.fa-ellipsis-h').click(function(){// 나중에 append된 코드에 적용이 안 되네 후
+		$(this).next().show();
+		$(this).next().next().show();
+	});
+	
+	// Reply Register
 	$('#newReplyBtn').click(function(){
 		var data = {
 			reply: $('#reply').val(),
@@ -146,6 +167,56 @@ $(function(){
 	});
 	
 	
+	// Reply Modify
+	var replyVal;
+	$('.modReplyBtn').click(function(){// 나중에 append된 코드에 적용이 안 되네 후
+		var replies = $(this).closest('td').find('.replies');
+		replyVal = replies.val();
+		$('.replies').attr('readonly', true);
+		//$(this).closest('td').find('.replies').removeAttr('readonly');
+		replies.removeAttr('readonly');
+		replies.focus();
+	});
+	
+	
+	$('.submitReplyBtn').click(function(){
+		var data = {
+			no: Number($(this).closest('td').find('.reply_no').val()),
+			reply: $(this).closest('td').find('.replies').val()
+		};
+		console.log(data);
+		console.log(data.no);
+	
+		$.ajax(root + '/nreplies/' + data.no, {
+			type: 'POST',
+			contentType: 'application/json',
+			data: JSON.stringify(data)
+		}).done(function(data){
+			console.log("success: " + data);
+			
+			getReplyList();	
+			
+		}).fail(function(err){
+			console.log(err);
+		});
+	});
+	
+	
+	// Reply Remove
+	$('.delReplyBtn').click(function(){
+		var reply_no = Number($(this).closest('td').find('.reply_no').val());
+		
+		$.ajax(root + '/nreplies/' + reply_no, {
+			type: 'DELETE'
+		}).done(function(data){
+			console.log("success: " + data);
+			
+			getReplyList();	
+			
+		}).fail(function(err){
+			console.log(err);
+		});
+	});
 	
 	
 });
